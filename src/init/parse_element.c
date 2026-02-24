5d3e898950813ee238be963447038de864d0ff18
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2026-02-24 16:13:09 +0800

    wip parse_element

--- src/init/parse_element.c
+++ src/init/parse_element.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 13:25:04 by kchiang           #+#    #+#             */
-/*   Updated: 2026/02/24 14:02:12 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/02/24 16:12:51 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@
 void	parse_element(t_map *map, char *line)
 {
 	if (!ft_strncmp(line, "NO ", 3))
-		parse_value(map, line + 3, 'N');
+		get_path(map, line + 3, NORTH);
 	else if (!ft_strncmp(line, "SO ", 3))
-		parse_value(map, line + 3, 'S');
+		get_path(map, line + 3, SOUTH);
 	else if (!ft_strncmp(line, "WE ", 3))
-		parse_value(map, line + 3, 'W');
+		get_path(map, line + 3, WEST);
 	else if (!ft_strncmp(line, "EA ", 3))
-		parse_value(map, line + 3, 'E');
+		get_path(map, line + 3, EAST);
 	else if (!ft_strncmp(line, "F ", 2))
-		parse_value(map, line + 2, 'F');
+		get_color(map, line + 2, 'F');
 	else if (!ft_strncmp(line, "C ", 2))
-		parse_value(map, line + 2, 'C');
+		get_color(map, line + 2, 'C');
 	else if (!map->bgcolor_set[CEILING] || !map->bgcolor_set[FLOOR]
 		|| !map->textures[NORTH] || !map->textures[SOUTH]
 	 	|| !map->textures[EAST] || !map->textures[WEST])
@@ -35,10 +35,20 @@ void	parse_element(t_map *map, char *line)
 	return ;
 }
 
-void	parse_value(t_map *map, char *str, char type)
+static void	get_path(t_map *map, char *str, t_direction direction)
 {
 	if (!*str)
 	{
 		destroy_map(map);
 		error_exit("Error\ncub3d: Invalid config values");
+	}
+}
+
+static void	get_color(t_map *map, char *str, char type)
+{
+	if (!*str)
+	{
+		destroy_map(map);
+		error_exit("Error\ncub3d: Invalid config values");
+	}
 }
