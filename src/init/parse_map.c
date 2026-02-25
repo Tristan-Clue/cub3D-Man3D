324d3250bc87f6f522f222ee106c23beb7024d6c
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2026-02-25 15:40:58 +0800

    -renamed parse_element to parse_tex -parse_map WIP (updated logic on parsing layout and reorganized functions)

--- src/init/parse_map.c
+++ src/init/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 08:06:39 by kchiang           #+#    #+#             */
-/*   Updated: 2026/02/24 13:42:10 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/02/25 15:38:16 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,31 +19,23 @@
  * TODO: Element tokenizing
 * */
 
-void		parse_element(t_map *map, char *line);
+int			parse_tex(t_map *map, char *line, int fd);
 
-static int	open_file(const char *s, const char *format);
+static void	get_elements(int fd, t_map *map);
 
 void	parse_map(t_map *map, const char *s)
-{
-	int		fd;
-	
-	fd = open_file(s, ".cub");
-	get_elements(fd, map);
-	close(fd);
-	return ;
-}
-
-static int	open_file(const char *s, const char *format)
 {
 	const char	*ext = ft_strrchr(s, '.');
 	int			fd;
 
-	if (!ext || ft_strcmp(ext, format))
+	if (!ext || ft_strcmp(ext, ".cub"))
 		error_exit("Error\ncub3d: invalid config filename");
 	fd = open(s, O_RDONLY);
 	if (fd == -1)
 		perror_exit("Error\ncub3d");
-	return (fd);
+	get_elements(fd, map);
+	close(fd);
+	return ;
 }
 
 static void	get_elements(int fd, t_map *map)
@@ -54,10 +46,11 @@ static void	get_elements(int fd, t_map *map)
 	line = get_next_line(fd);
 	while (line)
 	{
-		if (ft_strcmp(line, "\n"))
-			parse_element(map, line);
+		if (ft_strcmp(line, "\n") && parse_tex(map, line, fd))
+			break ;
 		free(line);
 		line = get_next_line(fd);
 	}
-	if ()
+	parse_layout(map, line, fd);
+	return ;
 }
--- src/init/parse_tex.c
+++ src/init/parse_tex.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse_element.c                                    :+:      :+:    :+:   */
+/*   parse_tex.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 13:25:04 by kchiang           #+#    #+#             */
-/*   Updated: 2026/02/25 15:19:42 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/02/25 15:38:19 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,63 @@
 #include "cub3d.h"
 #include <stdbool.h>
 
-void	parse_element(t_map *map, char *line)
+static void	get_path(t_map *map, char *str, t_direction direction, int fd);
+static void	get_color(t_map *map, char *str, char type, int fd);
+
+int	parse_tex(t_map *map, char *line, int fd)
 {
 	if (!ft_strncmp(line, "NO ", 3))
-		get_path(map, line + 3, NORTH);
+		get_path(map, line + 3, NORTH, fd);
 	else if (!ft_strncmp(line, "SO ", 3))
-		get_path(map, line + 3, SOUTH);
+		get_path(map, line + 3, SOUTH, fd);
 	else if (!ft_strncmp(line, "WE ", 3))
-		get_path(map, line + 3, WEST);
+		get_path(map, line + 3, WEST, fd);
 	else if (!ft_strncmp(line, "EA ", 3))
-		get_path(map, line + 3, EAST);
+		get_path(map, line + 3, EAST, fd);
 	else if (!ft_strncmp(line, "F ", 2))
-		get_color(map, line + 2, 'F');
+		get_color(map, line + 2, 'F', fd);
 	else if (!ft_strncmp(line, "C ", 2))
-		get_color(map, line + 2, 'C');
+		get_color(map, line + 2, 'C', fd);
 	else if (!map->bgcolor_set[CEILING] || !map->bgcolor_set[FLOOR]
 		|| !map->textures[NORTH] || !map->textures[SOUTH]
-	 	|| !map->textures[EAST] || !map->textures[WEST])
-		error_exit("Error\ncub3d: Invalid config settings");
-	return ;
+		|| !map->textures[EAST] || !map->textures[WEST])
+	{
+		close(fd);
+		destroy_map(map);
+		error_exit("Error\ncub3d: Invalid or incomplete configs");
+	}
+	else
+		return (1);
+	return (0);
 }
 
-static void	get_path(t_map *map, char *str, t_direction direction)
+static void	get_path(t_map *map, char *str, t_direction direction, int fd)
 {
-	int	fd;
+	int	texfd;
 
-	fd = open(str, O_RDONLY);
-	if (fd == -1)
+	texfd = open(str, O_RDONLY);
+	if (texfd == -1)
 	{
+		close(fd);
 		destroy_map(map);
 		perror_exit("Error\ncub3d");
 	}
-	close(fd);
+	close(texfd);
 	map->textures[direction] = ft_strdup(str);
 	if (!map->textures[direction])
 	{
+		close(fd);
 		destroy_map(map);
 		perror_exit("Error\ncub3d");
 	}
 	return ;
 }
 
-static void	get_color(t_map *map, char *str, char type)
+static void	get_color(t_map *map, char *str, char type, int fd)
 {
 	if (!*str)
 	{
+		close(fd);
 		destroy_map(map);
 		error_exit("Error\ncub3d: Invalid bgcolor values");
 	}
