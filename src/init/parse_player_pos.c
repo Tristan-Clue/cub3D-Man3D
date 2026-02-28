e79eb63bed6e7e02ea9938903a4247c351f00988
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2026-02-28 19:16:59 +0800

    finished parse_player_pos

--- src/init/parse_player_pos.c
+++ src/init/parse_player_pos.c
@@ -6,13 +6,14 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 17:19:01 by kchiang           #+#    #+#             */
-/*   Updated: 2026/02/28 17:30:08 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/02/28 19:12:01 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "cub3d.h"
-#include <stdbool.h>
+
+static int	is_direction(char c);
+static void	parse_pos(t_map *map, int row, int col);
 
 void	parse_player_pos(t_map *map)
 {
@@ -39,4 +40,23 @@ void	parse_player_pos(t_map *map)
 		}
 		row++;
 	}
+}
+
+static int	is_direction(char c)
+{
+	return (c == 'N' || c == 'S' || c == 'E' || c == 'W');
+}
+
+static void	parse_pos(t_map *map, int row, int col)
+{
+	map->starting_pos.x = col;
+	map->starting_pos.y = row;
+	if (map->layout[row][col] == 'N')
+		map->starting_dir = NORTH;
+	else if (map->layout[row][col] == 'S')
+		map->starting_dir = SOUTH;
+	else if (map->layout[row][col] == 'E')
+		map->starting_dir = EAST;
+	else if (map->layout[row][col] == 'W')
+		map->starting_dir = WEST;
 }
