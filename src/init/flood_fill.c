533f6b20778d21c789876c25a8f6f161dd6c19f6
Kai Zhi Chiang <kchiang@u81z10s01.42kl.edu.my>
2026-03-02 14:35:48 +0800

    finished mask_has_error and updated flood_fill scan_wall to recall scan_space when encountering '0'

--- src/init/flood_fill.c
+++ src/init/flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/01 15:46:35 by kchiang           #+#    #+#             */
-/*   Updated: 2026/03/01 19:56:49 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/03/02 14:25:53 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,11 @@ static int	scan_wall(t_map *map, char mask[][MAX_MAP_SIZE], int row, int col)
 	if (row < 0 || row == map->row_number || col < 0 || col == map->col_number
 		|| mask[row][col] == true || map->layout[row][col] == ' ')
 		return (SUCCESS);
-	if ((row == 0 || row == map->row_number - 1 || col == 0
-		|| col == map->col_number - 1) && map->layout[row][col] == '0')
+	if (row == 0 || row == map->row_number - 1
+		|| col == 0 || col == map->col_number - 1)
 		return (FAILURE);
+	if (map->layout[row][col] == '0')
+		return (scan_space(map, mask, row, col));
 	if (map->layout[row][col] == '1')
 	{
 		mask[row][col] == true;
