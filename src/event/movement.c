bfcc30e2419c05c5f1a0bbbc1af0133b494205e6
Tristan <123tristanma@gmail.com>
2026-03-06 15:07:02 +0800

    Added 0.2 wall collision for movements

--- src/event/movement.c
+++ src/event/movement.c
@@ -6,7 +6,7 @@
 /*   By: mjoon-yu <mjoon-yu@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 20:20:39 by mjoon-yu          #+#    #+#             */
-/*   Updated: 2026/03/06 14:24:58 by mjoon-yu         ###   ########.fr       */
+/*   Updated: 2026/03/06 15:05:43 by mjoon-yu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,15 @@ static void	movement_w(t_player *player, t_map *map)
 	double	x;
 	double	y;
 
-	//printf("here\n");
 	x = player->pos.x + player->dir.x * MOVE_SPEED;
 	y = player->pos.y + player->dir.y * MOVE_SPEED;
-	if (map->layout[(int)floor(player->pos.y)][(int)floor(x)] != '1')
-	{
+	if (player->dir.x && map->layout
+		[(int)floor(player->pos.y)]
+		[(int)floor(x + (player->dir.x / fabs(player->dir.x)) * 0.2)] != '1')
 		player->pos.x = x;
-	}
-	if (map->layout[(int)floor(y)][(int)floor(player->pos.x)] != '1')
+	if (player->dir.y && map->layout
+		[(int)floor(y + (player->dir.y / fabs(player->dir.y)) * 0.2)]
+		[(int)floor(player->pos.x)] != '1')
 		player->pos.y = y;
 }
 
@@ -37,9 +38,13 @@ static void	movement_s(t_player *player, t_map *map)
 
 	x = player->pos.x - player->dir.x * MOVE_SPEED;
 	y = player->pos.y - player->dir.y * MOVE_SPEED;
-	if (map->layout[(int)floor(player->pos.y)][(int)floor(x)] != '1')
+	if (player->dir.x && map->layout
+		[(int)floor(player->pos.y)]
+		[(int)floor(x - (player->dir.x / fabs(player->dir.x)) * 0.2)] != '1')
 		player->pos.x = x;
-	if (map->layout[(int)floor(y)][(int)floor(player->pos.x)] != '1')
+	if (player->dir.y && map->layout
+		[(int)floor(y - (player->dir.y / fabs(player->dir.y)) * 0.2)]
+		[(int)floor(player->pos.x)] != '1')
 		player->pos.y = y;
 }
 
@@ -49,9 +54,13 @@ static void	movement_a(t_player *player, t_map *map)
 	double	y;
 	x = player->pos.x - player->plane.x * MOVE_SPEED;
 	y = player->pos.y - player->plane.y * MOVE_SPEED;
-	if (map->layout[(int)floor(player->pos.y)][(int)floor(x)] != '1')
+	if (player->plane.x && map->layout
+		[(int)floor(player->pos.y)]
+		[(int)floor(x - (player->plane.x / fabs(player->plane.x)) * 0.2)] != '1')
 		player->pos.x = x;
-	if (map->layout[(int)floor(y)][(int)floor(player->pos.x)] != '1')
+	if (player->plane.y && map->layout
+		[(int)floor(y - (player->plane.y / fabs(player->plane.y))* 0.2)]
+		[(int)floor(player->pos.x)] != '1')
 		player->pos.y = y;
 }
 
@@ -62,9 +71,13 @@ static void	movement_d(t_player *player, t_map *map)
 
 	x = player->pos.x + player->plane.x * MOVE_SPEED;
 	y = player->pos.y + player->plane.y * MOVE_SPEED;
-	if (map->layout[(int)floor(player->pos.y)][(int)floor(x)] != '1')
+	if (player->plane.x && map->layout
+		[(int)floor(player->pos.y)]
+		[(int)floor(x + (player->plane.x / fabs(player->plane.x)) * 0.2)] != '1')
 		player->pos.x = x;
-	if (map->layout[(int)floor(y)][(int)floor(player->pos.x)] != '1')
+	if (player->plane.y && map->layout
+		[(int)floor(y + (player->plane.y / fabs(player->plane.y)) * 0.2)]
+		[(int)floor(player->pos.x)] != '1')
 		player->pos.y = y;
 }
 
@@ -85,8 +98,8 @@ int	diagonal_movement(int movement)
 
 void	handle_movement(t_input *input, t_player *player, t_map *map)
 {
-	if (diagonal_movement(input->movement))
-		perform_diagonal(input, player, map);
+//	if (diagonal_movement(input->movement))
+//		perform_diagonal(input, player, map);
 	if (input->movement & KEY_W)
 		movement_w(player, map);
 	if (input->movement & KEY_S)
