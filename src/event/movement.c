55130ccf7fdfec209f88f1920aa5c03e56e9f594
Tristan <123tristanma@gmail.com>
2026-03-14 14:23:50 +0800

    Added a version with more checks, breaking more shit haahahahahaahhah

--- src/event/movement.c
+++ src/event/movement.c
@@ -6,7 +6,7 @@
 /*   By: mjoon-yu <mjoon-yu@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 20:20:39 by mjoon-yu          #+#    #+#             */
-/*   Updated: 2026/03/13 21:23:40 by mjoon-yu         ###   ########.fr       */
+/*   Updated: 2026/03/14 14:18:53 by mjoon-yu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,38 +21,6 @@ double	get_sign(double i)
 	if (i < 0)
 		return (-1);
 	return (0);
-}
-
-int	check_x(double x, t_player *player, t_map *map)
-{
-	if (map->layout[(int)floor(player->pos.y)]
-		[(int)floor(x + get_sign(player->dir.x) * 0.2)] == '1'
-			|| map->layout[(int)floor(player->pos.y)]
-			[(int)floor(x - get_sign(player->dir.x) * 0.2)] == '1')
-			return (0);
-		if (map->layout[(int)floor(player->pos.y)]
-			[(int)floor(x - get_sign(player->plane.x) * 0.2)] == '1'
-			|| map->layout[(int)floor(player->pos.y)]
-			[(int)floor(x + get_sign(player->plane.x) * 0.2)] == '1')
-			return (0);
-	return (1);
-}
-
-int	check_y(double y, t_player *player, t_map *map)
-{
-		if (map->layout[(int)floor(y + get_sign(player->dir.y) * 0.2)]
-			[(int)floor(player->pos.x)] == '1' || map->layout
-			[(int)floor(y - get_sign(player->dir.y) * 0.2)]
-			[(int)floor(player->pos.x)] == '1')
-			return (0);
-		if (map->layout
-			[(int)floor(y - get_sign(player->plane.y) * 0.2)]
-			[(int)floor(player->pos.x)] == '1'
-			|| map->layout
-			[(int)floor(y + get_sign(player->plane.y) * 0.2)]
-			[(int)floor(player->pos.x)] == '1')
-			return (0);
-	return (1);
 }
 
 int	collide_y(double x, t_player *player, t_map *map)
@@ -84,6 +52,59 @@ int	collide_x(double y, t_player *player, t_map *map)
 			return (0);
 	return (1);
 }
+
+int	check_x(double x, t_player *player, t_map *map)
+{
+	double	movement_dir;
+	double	movement_plane;
+
+	movement_dir = get_sign(player->dir.x) * 0.2;
+	movement_plane = get_sign(player->plane.x) * 0.2;
+	if (map->layout[(int)floor(player->pos.y)]
+		[(int)floor(x + movement_dir)] == '1'
+		|| !collide_y(x + movement_dir, player, map))
+		return (0);
+	if (map->layout[(int)floor(player->pos.y)]
+		[(int)floor(x - movement_dir)] == '1'
+		|| !collide_y(x - movement_dir, player, map))
+		return (0);
+	if (map->layout[(int)floor(player->pos.y)]
+		[(int)floor(x - movement_plane)] == '1'
+		|| !collide_y(x - movement_plane, player, map))
+		return (0);
+	if (map->layout[(int)floor(player->pos.y)]
+		[(int)floor(x + movement_plane)] == '1'
+		|| !collide_y(x + movement_plane, player, map))
+		return (0);
+	return (1);
+}
+
+int	check_y(double y, t_player *player, t_map *map)
+{
+	double	movement_dir;
+	double	movement_plane;
+
+	movement_dir = get_sign(player->dir.y) * 0.2;
+	movement_plane = get_sign(player->plane.y) * 0.2;
+	if (map->layout[(int)floor(y + movement_dir)]
+		[(int)floor(player->pos.x)] == '1' 
+		|| !collide_x(y + movement_dir, player, map))
+		return (0);
+	if (map->layout [(int)floor(y - movement_dir)]
+		[(int)floor(player->pos.x)] == '1'
+		|| !collide_x(y - movement_dir, player, map))
+		return (0);
+	if (map->layout[(int)floor(y - movement_plane)]
+		[(int)floor(player->pos.x)] == '1'
+		|| !collide_x(y - movement_plane, player, map))
+		return (0);
+	if (map->layout[(int)floor(y + movement_plane)]
+		[(int)floor(player->pos.x)] == '1'
+		|| !collide_x(y + movement_plane, player, map))
+		return (0);
+	return (1);
+}
+
 t_vec	combine_movement(t_input *input, t_player *player)
 {
 	t_vec	movement;
@@ -118,6 +139,8 @@ t_vec	combine_movement(t_input *input, t_player *player)
 	return (movement);
 }
 
+// TODO: Start from the base of walking side ways and hitting a corner
+// diagonal
 void	handle_movement(t_input *input, t_player *player, t_map *map)
 {
 	t_vec	movement;
@@ -129,12 +152,12 @@ void	handle_movement(t_input *input, t_player *player, t_map *map)
 		movement = combine_movement(input, player);
 		new.x = player->pos.x + movement.x;
 		new.y = player->pos.y + movement.y;
-		if (check_x(new.x, player, map) && collide_y(new.x, player, map))
+		if (check_x(new.x, player, map))
 		{
 			check = 1;
 			player->pos.x = new.x;
 		}
-		if (check_y(new.y, player, map) && collide_x(new.y, player, map))
+		if (check_y(new.y, player, map))
 		{
 			check = 1;
 			player->pos.y = new.y;
